eb5a152f6b2e722d0db421798c0cc0a4f4880d67
gregcorby <greg.corby@gmail.com>
2021-06-13 21:07:38 -0700

    first commit

---
+++ src/style/Global.ts
@@ -0,0 +1,113 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${({ theme }) => theme.colors.background};
+    font-family: sans-serif !important; 
+    img {
+      height: auto;
+      max-width: 100%;
+    }
+    .dVGJSp {
+        background-image: url(https://secureservercdn.net/104.238.68.196/6hj.ede.myftpupload.com/wp-content/uploads/2021/05/cannabis.jpg?time=1622938058);
+    
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: cover;
+        min-height: 100vh;
+    }
+    .dVGJSp {
+        padding: 0 !important;
+
+    }
+    .czVxJH {
+        margin: 0 !important;
+        width: 100%;
+        max-width: 100%;
+    }
+    .hLlXtp {
+        border-radius: 12px !important;
+        padding: 0;
+        position: static;
+    }
+    .hLlXtp:before {
+        content: "";
+        position: absolute;
+        display: block;
+        background: url(https://secureservercdn.net/104.238.68.196/6hj.ede.myftpupload.com/wp-content/uploads/ct-logos/logo_156f6f9…_1x.png);
+        height: 80px;
+        width: 80px;
+        top: 40px;
+        background-repeat: no-repeat;
+        background-size: 80px auto;
+    }
+    .eIbyFk, .sc-gsTCUz {
+        color: #000 !Important;
+        font-family: sans-serif !important; 
+    } 
+    .fsbHO {
+        font-family: sans-serif !important; 
+        background-color: #17b541;
+        border-radius: 8px;
+    }
+    .bJeyPI, .dTwbCo {
+        padding: 0 !important;
+        border-radius: 0 !important;
+    }
+    .iMRokR {
+        border-radius: 0px;
+        background-color: #f8f8f8;
+        box-shadow: none !important;
+        border-bottom: 1px solid rgba(0,0,0,.05);
+        padding: 0 12px;
+    }
+    .kTrCXz {
+       fill: #17b541;
+    }
+    .grJRBF {
+        color: #17b541;
+    }
+    .fa-dDHS {
+        display: grid;
+        grid-auto-rows: auto;
+        grid-row-gap: 0;
+    }
+    .cSbxey {
+        display: grid;
+        grid-auto-rows: auto;
+        position: absolute;
+        z-index: 9999;
+        left: 50%;
+        transform: translateX(-50%) translateY(-50%);
+        top: 50%;
+    }
+    button.sc-dlfnbm.kqVKBK.sc-hKgILt.hFmlgK {
+        border-radius: 50%;
+        background: #fff;
+        z-index: 9999;
+        box-shadow: 0 2px 2px 0px rgb(0 0 0 / 10%);
+    }   
+    .hjqBrC {
+        margin-top: 0;
+        padding: 24px;
+    }
+    .lmyila {
+        color: #000;
+    }    
+    .dpYQHO {
+        display: none;
+    }
+    a.sc-eLgOdN.huihyY {
+    background: url(https://secureservercdn.net/104.238.68.196/6hj.ede.myftpupload.com/wp-content/uploads/ct-logos/logo_156f6f9…_2x.png);
+    background-size: 50PX;
+    WIDTH: 50px;
+    height: 50px;
+    }
+}
+    a.sc-eLgOdN.huihyY svg {
+        display: none;
+    }
+  }
+`
+
+export default GlobalStyle
